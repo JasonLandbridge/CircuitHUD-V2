28b18c1cbf4a61b8bfacfffd63a940d79e24414d
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-03-07 12:55:27 +0100

    removed log function as it caused a crash

--- util/log.lua
+++ util/log.lua
@@ -1,11 +1,3 @@
-local Is = require("__stdlib__/stdlib/utils/is")
-
--- Logs the message to the player in the console.
--- @param player_index LuaPlayer
-function log(player_index, message)
-	get_player(player_index).print(message)
-end
-
 -- Logs the message to the player in the console only if debug_mode is enabled.
 -- @param player_index LuaPlayer
 function debug_log(player_index, message)
