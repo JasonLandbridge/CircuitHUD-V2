35deaf47976c3253418117551a19e4ec272992d8
JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>
2021-03-08 18:25:46 +0100

    Refactored the recipe and signals to their own file, from data.lua

---
+++ prototypes/signals.lua
@@ -0,0 +1,20 @@
+--
+-- SIGNALS
+--
+data:extend {
+	{
+		type = "item-subgroup",
+		name = "circuit-hid-signals",
+		group = "signals",
+		order = "f"
+	},
+	{
+		type = "virtual-signal",
+		name = "signal-hide-hud-comparator",
+		icon = "__CircuitHUD-V2__/graphics/icon/signal/signal-hide-hud-comparator.png",
+		icon_size = 64,
+		icon_mipmaps = 4,
+		subgroup = "circuit-hid-signals",
+		order = "d[hud-comparator]-[hide]"
+	}
+}
